35a6d1b8d7aa129cf21bbbfc242d79205b709a3d
sreejithdikru <imsreejithofficial@gmail.com>
2021-06-14 23:05:56 +0530

    added a new method

--- scrape.py
+++ scrape.py
@@ -4,16 +4,18 @@ import datetime
 now = datetime.datetime.now()
 year = now.year
 
-def url_to_file(url, file_name= 'world.html'):
+def url_to_txt(url, file_name= 'world.html', save = False):
     r = requests.get(url)
     if r.status_code == 200:
         html_text = r.text
-        with open(f"world-{year}.html", 'w') as f:
-            f.write(html_text)
-            return html_text
-        return ""
-
+        if save:
+            with open(f"world-{year}.html" , 'w') as f:
+                f.write(html_text)
+        return html_text
+    return ""
 
 
 url = "https://www.boxofficemojo.com/"
-url_to_file(url)
+
+html_text = url_to_txt(url)
+
